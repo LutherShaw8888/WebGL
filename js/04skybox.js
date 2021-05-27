01efffa48c62409accde7bb6af77b13ebbeb2985
Luther <luther88@gmail.com>
2021-05-27 04:10:27 +0000

    add raycaster

--- js/04skybox.js
+++ js/04skybox.js
@@ -1,4 +1,4 @@
-var scene, camera, renderer;
+var scene, camera, renderer,videoNode,videoTexture;
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 1, 2);
@@ -56,12 +56,14 @@ function bgMusic() {
 // sphere mode
 function panoVideoMode() {
     // let videoNode = document.getElementById('video');
-    let videoNode = document.createElement('video');
-    videoNode.src="res/video/video01.mp4";
+     videoNode = document.createElement('video');
+    videoNode.src = "../res/video/video01.mp4";
     videoNode.autoplay = true;
     videoNode.loop = true;
-    let videoTexture = new THREE.VideoTexture(videoNode);
- 
+    videoNode.preload = "auto";
+    videoNode.autoload = true;
+     videoTexture = new THREE.VideoTexture(videoNode);
+
     var sphere = new THREE.SphereGeometry(10, 10, 10);
     var mat = new THREE.MeshBasicMaterial({ map: videoTexture, side: THREE.DoubleSide });
     var mesh = new THREE.Mesh(sphere, mat);
@@ -70,6 +72,15 @@ function panoVideoMode() {
 }
 panoVideoMode();
 
+
+
+
+// click to change video
+function changeVideo(src) {
+    videoNode.src = src;
+	videoTexture = new THREE.VideoTexture(videoNode);
+	videoTexture.needsUpdate = true;
+}
 
 
 
@@ -82,6 +93,58 @@ var orbit = new THREE.OrbitControls(camera, renderer.domElement);
 orbit.autoRotate = true;
 orbit.enableDamp = true;
 orbit.autoRotateSpeed = 0.1;
+
+
+// add sprite
+let uiTex_1 = new THREE.TextureLoader().load('../res/ui/LuBiao.png');
+let uiMat_1 = new THREE.SpriteMaterial({
+	map: uiTex_1
+});
+let ui_1 = new THREE.Sprite(uiMat_1);
+ui_1.name="btn";
+ui_1.position.set(5, 1, 0);
+scene.add(ui_1);
+
+
+window.addEventListener("click", event => {
+    event.preventDefault();
+    let raycaster = new THREE.Raycaster();
+    let mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;//threejs坐标点的标准化
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+    let intersects = raycaster.intersectObjects(scene.children);
+    intersects.forEach(child=>{
+        if(child.object instanceof THREE.Sprite){
+            console.log('aaaaa',child.object.name);
+            // changeVideo('../res/video/video02.mp4');
+	}
+	})
+
+
+	// if (intersects.length > 0) {
+	// 	let this_sel = intersects[0];
+	// 	if (this_sel.object.type == 'Sprite') {
+	// 		console.log('aaaaa')
+	// 		window.open('https:720yun.com');
+    //         changeVideo('../res/video/video02.mp4');
+    //         console.log("clientX:" + event.clientX);
+    //         console.log("clientY:" + event.clientY);
+	// 	}
+	// }
+
+}, false);
+
+
+
+// 窗口变动触发的方法
+window.addEventListener('resize', onResize, false);
+function onResize() {
+    console.log('窗口缩放');
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 
 function init() {
